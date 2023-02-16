f0e0d4d963bce368b6a091f1a65df72ad9b186df
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-16 14:37:13 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,12 +2,11 @@
 
 /**
 *main - Entry point
-*main - prints "with proper grammar...
 *Return: Always 0 (Success)
 */
 
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }

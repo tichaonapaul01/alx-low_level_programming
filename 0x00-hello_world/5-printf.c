680d878c1b06f081057e6fe578e8b25d7eeb88a9
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-16 13:57:34 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+* * *  main - Prints "with good grammar ...
+*Return: Always 0.
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art");
+Return 0;
+}

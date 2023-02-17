87b23a993e9abe41b7ca427475bf4d1444f75042
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-17 13:52:31 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,6 @@
-#include <std1ib.h>
+#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
 * main - main block
 * Desription: get a random number and print that number
@@ -15,16 +14,10 @@ int n;
 srand(time(0));
 n=rand() - RAND_MAX / 2;
 if (n > 0)
-{
 printf("%d is positive\n", n);
-}
 else if (n < 0)
-{
 printf("%i is nagative\n", n);
-}
 else
-{
 printf("%i is zero\n", n);
-}
 return (0);
 }

a52bc3c1cab34d64f0e29b35d0be9945c7b6ec98
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-18 12:16:11 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+*main - main block
+*Description: print the alphabet in lower case
+*and the in uppercase, follow by a new line
+*Return: 0
+*/
+int main(void)
+{
+char c = 'a';
+while (c <= '2')
+{
+putchar(c);
+c++;
+}
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n')
+return (0);
+}

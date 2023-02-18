f681b292af6c016c28cdbd062d0f17c2fe047235
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-18 09:57:51 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,8 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
-*main - Entry point
-*Return: always 0 (Success)
+* main - main block
+* Description: Get a random number and print the number
+* and if it is positive, nagative, or zero
+* Return: always 0 (Success)
 */
 int main(void)
 {
@@ -11,13 +13,12 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	// true if the number is greater than zero
 	if (n > 0)
 		printf("%d is positive.\n", n);
 	else if (n < 0)
 		printf("%d is nagative.\n", n);
 	else
-	    printf(%d is zero.\n" n);
+	    printf("%d is zero.\n" n);
 
 	return (0);
 }

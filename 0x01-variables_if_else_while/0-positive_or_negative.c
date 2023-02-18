42372cb296e243693753925384d2f28a19f02c2d
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-18 10:16:28 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,7 +16,7 @@ n = rand() - RAND_MAX / 2;
 if (n > 0)
 printf("%d is positive\n", n);
 else if (n < 0)
-printf("%d is nagative\n", n);
+printf("Result: %d is nagative\n", n);
 else
 printf("%d is zero\n", n);
 return (0);

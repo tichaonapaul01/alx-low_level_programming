3e39b886d39f25bbdcd66648b9fa1a9a6d0a69c7
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-19 03:40:25 -0800

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints number combinations in ascending order
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i < 9)
+		{
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}

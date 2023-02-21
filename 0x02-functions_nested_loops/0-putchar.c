36aa95681095f6f3ee6d4823b56f51dba09429c8
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-02-21 12:23:19 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+}
+
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+0x02. C - Functions, nested loops have more task needs a clear understanding to be solved!

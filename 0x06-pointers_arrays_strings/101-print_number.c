0ea399cacad1ccb67d681066b6ad0ac5303cf56e
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-03-02 12:30:57 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,15 +1,48 @@
 #include "main.h"
 
 /**
- * main - the function that print an integer.
- *
- * Return: Always 0.
+ * print_number - Function that prints an integer.
+ * @n: int type number
+ * Description: Can only use _putchar to print.
  */
-int main(void)
+void print_number(int n)
 {
-    print_number(5);
-    _putchar('\n');
-    print_number(7);
-    _putchar('\n');
-    return (0);
+	long m; // power of 10 
+	int c; // boolean check 
+	long num; // convert int to long 
+
+	num = n;
+	// negatives 
+	if (num < 0)
+	{
+		num *= -1;
+		_putchar('-');
+	}
+
+	// count up 
+	m = 1;
+	c = 1;
+	while (c)
+	{
+		if (num / (m * 10) > 0)
+			m *= 10;
+		else
+			c = 0;
+	}
+
+	// count down
+	while (num >= 0)
+	{
+		if (m == 1)
+		{
+			_putchar(num % 10 + '0');
+			num = -1;
+		}
+		else
+		{
+			_putchar((num / m % 10) + '0');
+			m /= 10;
+		}
+	}
 }
+

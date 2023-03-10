58d6dd1eff6c3833986f09862e10ca47fa86c232
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-03-10 15:07:12 -0800

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,6 +13,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", tich(argv[1]) * tich(argv[2]));
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
 	return (0);
 }

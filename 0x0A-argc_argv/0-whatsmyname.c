35eb99a2dba3e70b0f607804aea79c653f3eee1e
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-03-10 14:44:46 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  * @argv: array that contains the program command line arguments.
  * Return: 0 - success.
  */
-int main(int argc, char *argv[])
+int main(int argc _attribute_((unused)), char *argv[])
 {
-	(void)argc;
 	printf("%s\n", argv[0];
 	return (0);
 }

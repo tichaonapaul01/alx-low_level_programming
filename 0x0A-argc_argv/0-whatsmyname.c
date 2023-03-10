6dc8275ad3d0abaa11fbe9ac60169fd1e74cce12
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-03-10 14:36:38 -0800

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,16 +1,13 @@
+#include <stdio.h>
 /**
-  * 0-whatsmyname.c - copy memory area
-  * @program: prints its name followed by a new line
-  * @print the new name without compiling
-  * @do not remove the path of the program
-  *
-  * Return:always (0)
-  */
-
-int main(int argc,  char *argv[])
+ * main - prints its name, followed by a new line.
+ * @argc: number of command line arguments.
+ * @argv: array that contains the program command line arguments.
+ * Return: 0 - success.
+ */
+int main(int argc, char *argv[])
 {
-          (void)argc; 
-          printf("%s\n", argv[0]);
-	  
-	  return (0);
+	(void)argc;
+	printf("%s\n", argv[0];
+	return (0);
 }

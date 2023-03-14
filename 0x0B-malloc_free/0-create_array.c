c184e25955198a28ce6de7e1be1677d73dbb366e
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-03-14 13:14:35 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -8,16 +8,16 @@
  * Return: char pointer to malloc created memory address or NULL if error
  */
 
-char *create_array(unsigned int size, char c);
+char *create_array(unsigned int size, char c)
 {
-	char = *a,
-	unsigned int;
+	char *a;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 
-	a = malloc( size * sizeof(*a));
-	if (a ==NULL)
+	a = malloc(size * sizeof(*a));
+	if (a == NULL)
 		return (NULL);
 
 	for (i = 0; i < size; i++)

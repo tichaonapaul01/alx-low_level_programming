81fd81feb3e11c3abadbf5a0678f92b118bae3a8
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-04 15:18:13 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,12 +2,15 @@
 
 /**
  * print_listint - prints all the elements of a list.
+ * @h: head of a list.
  *
  * Return: numbers of nodes.
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nnodes = 0;
+	size_t nnodes;
+
+	nnodes = 0;
 
 	while (h != NULL)
 	{

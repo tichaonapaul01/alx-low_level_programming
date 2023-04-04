16ab317f85ffa3d64e6b1b631df898b4479dff3c
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-04 15:04:16 -0700

    1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,15 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t num_of_nodes;
+	size_t nnodes;
        
-	num_of_nodes = 0;
+	nnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num_of_nodes++;
+		nnodes++;
 	}
-	return (num_of_nodes);
+	return (nnodes);
 }

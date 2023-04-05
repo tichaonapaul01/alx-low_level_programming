6d5430f6716afec434e08c6a5ed747e77d73e573
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-05 14:29:23 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,7 @@
  * @head: head of a list.
  * @index: index of the node.
  *
- * Return: return NULL if the code does not exist.
+ * Return: nth node, or  NULL if the code does not exist.
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {

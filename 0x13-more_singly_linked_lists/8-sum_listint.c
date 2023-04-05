d909ec84a11c31be69e538b75f17fe65905faabd
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-05 15:00:03 -0700

    8-sum_listint.c

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -8,12 +8,12 @@
 int sum_listint(listint_t *head)
 {
 	int sum;
-	
+
 	sum = 0;
 	while (head != NULL)
 	{
-		sum = sum + head->n
-		head = head->next
+		sum += head->n;
+		head = head->next;
 	}
 
 	return (sum);

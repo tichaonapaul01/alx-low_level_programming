31b25978174518332db0c9f2f6bbf5af784acb39
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-05 12:55:00 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,7 +8,7 @@
 */
 void free_listint2(listint_t **head)
 {
-    listint_t *current;
+    listint_t *curr;
     listint_t *temp;
 
     if (head == NULL || *head == NULL)
@@ -16,8 +16,9 @@ void free_listint2(listint_t **head)
         return;
     }
 
-    current = *head;
-    while (current != NULL) {
+    curr = *head;
+    while (curr != NULL) 
+    {
         temp = curr;
         curr = curr->next;
         free(tmp);

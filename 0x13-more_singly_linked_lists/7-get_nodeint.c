e059c2a8ebe25530cb052c883e406a1b3a2e5c08
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-04-05 14:37:31 -0700

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -13,7 +13,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	for (i = 0; i < index && head != NULL; i++)
 	{
-		head = head->next
+		head = head->next;
 	}
 
 	return (head);

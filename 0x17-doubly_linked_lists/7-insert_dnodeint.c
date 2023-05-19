cf1c232df56883201afa4368ce80f910cefdd906
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-18 18:12:43 -0700

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,63 +1,59 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - Inserts a new node at a given position.
- * @h: A pointer to the head of the doubly linked list.
- * @idx: The index where the new node should be added.
- * @n: The data value of the new node.
+ * insert_dnodeint_at_index - inserts a new node at
+ * a given position
  *
- * Return: The address of the new node, or NULL if it failed.
+ * @h: head of the list
+ * @idx: index of the new node
+ * @n: value of the new node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-dlistint_t *new_node, *current_node;
-unsigned int i;
-if (h == NULL)
+dlistint_t *new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+{
 return (NULL);
+}
 
-/* Create a new node */
-new_node = malloc(sizeof(dlistint_t));
-if (new_node == NULL)
-return (NULL);
-new_node->n = n;
+new->n = n;
+new->prev = NULL;
+new->next = NULL;
 
-/* Insert at the beginning */
 if (idx == 0)
 {
-new_node->prev = NULL;
-new_node->next = *h;
+new->next = *h;
 if (*h != NULL)
-(*h)->prev = new_node;
-*h = new_node;
-return (new_node);
-}
-
-/* Traverse to the desired position */
-current_node = *h;
-for (i = 0; i < idx - 1 && current_node != NULL; i++)
-current_node = current_node->next;
-
-/* If index is out of bounds */
-if (current_node == NULL)
 {
-free(new_node);
+(*h)->prev = new;
+}
+*h = new;
+}
+else
+{
+dlistint_t *current = *h;
+unsigned int i = 0;
+while (i < idx - 1 && current != NULL)
+{
+current = current->next;
+i++;
+}
+if (current != NULL)
+{
+new->next = current->next;
+if (current->next != NULL)
+{
+current->next->prev = new;
+}
+current->next = new;
+new->prev = current;
+}
+else
+{
+free(new);
 return (NULL);
 }
-
-/* Insert at the end */
-if (current_node->next == NULL)
-{
-new_node->prev = current_node;
-new_node->next = NULL;
-current_node->next = new_node;
-return (new_node);
 }
-
-/* Insert in the middle */
-new_node->prev = current_node;
-new_node->next = current_node->next;
-current_node->next->prev = new_node;
-current_node->next = new_node;
-
-return (new_node);
+return (new);
 }

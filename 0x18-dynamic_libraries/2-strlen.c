ad6f8317fc3cc43cbeddb5fc8e091ebc398e8579
tichaonapaul01 <tichaonapaul01@gmail.com>
2023-05-22 15:21:16 -0700

    libdynamic.so

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen - Entry, count string function
+ *
+ * @s: This is the parameter/ argument to be counted
+ *
+ * Return: int on success
+ */
+
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count])
+	{
+		count++;
+	}
+	return (count);
+}
---
+++ 0x18-dynamic_libraries/README.md
@@ -0,0 +1,1 @@
+Project for Dynamic Library
